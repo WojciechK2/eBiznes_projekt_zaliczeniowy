28130610350d7b2a6240f20ce927ef1e2c8cdb32
Wojciech Klasa <klasa.1.wojciech@gmail.com>
2022-09-18 23:05:20 +0200

    fixing fetch

--- Frontend_React/src/Utils/fetchData.js
+++ Frontend_React/src/Utils/fetchData.js
@@ -1,6 +1,15 @@
 export async function fetchData(url) {
+
+    const init = {
+        mode: "cors",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+    }
+
     try {
-        return await fetch(url)
+        return await fetch(url,init)
             .then((response) => response.json())
     } catch (error) {
         return error
